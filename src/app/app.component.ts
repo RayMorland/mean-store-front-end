21df9d905d1746b4199412c8c9f4c5bf2dfdcaa9
RayMorland <raymondmorland@gmail.com>
2021-11-24 21:29:12 -0800

    Figured out storefront API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { shopifyToken } from './credentials';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mean-store-front-end';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    console.log('hello');
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'X-Shopify-Storefront-Access-Token': shopifyToken,
+    });
+    this.http
+      .post(
+        'https://morlandsdevstore.myshopify.com/api/graphql.json',
+        {
+          query:
+            'query{products (first: 5) {edges {node {id title description}}}}',
+        },
+        { headers: headers }
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
 }
